a74bc97dba47583d16e818aa839e4a981e3bd8aa
micawoken <contact@michaelwongmusic.com>
2026-06-19 21:36:35 -0400

    added gpl text to all files, and added a you indicator on list contributors

--- src/pages/api/v1/identity/activation.ts
+++ src/pages/api/v1/identity/activation.ts
@@ -7,6 +7,22 @@
  * user_activation permission so enrollment-capable users can bring accounts online, while
  * deactivation (which revokes an active user's access) remains admin-only.
  *
+ *
+ * Copyright (C) 2026 Michael Wong.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This license is also subject to additional terms as specified in the README.md.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 import type { APIContext, APIRoute } from "astro"
