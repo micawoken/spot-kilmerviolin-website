784339cc9374a6a8a96d71bdbbfd6ead2c27d429
micawoken <contact@michaelwongmusic.com>
2026-07-19 19:58:50 -0400

    bulk updated AGPL file/project text; added license.txt to Astro public, and edited license files

--- src/lib/compositor/entity-fields.ts
+++ src/lib/compositor/entity-fields.ts
@@ -21,9 +21,13 @@
  *
  * Copyright (C) 2026 Michael Wong.
  *
+ * This file is part of the spot-kilmerviolin-website program, available at 
+ * https://github.com/micawoken/spot-kilmerviolin-website.
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or any later version.
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
  *
  * This license is also subject to additional terms as specified in the README.md.
  *
